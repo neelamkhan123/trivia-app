0f8cd2ea2c1ff879050a5b7a516fd997bbd709d5
neelamkhan123 <neelamkhanwork@gmail.com>
2022-09-16 17:14:28 +0100

    mq

--- dist/main.77bb5cfd.js
+++ dist/main.77bb5cfd.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51383" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61237" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
